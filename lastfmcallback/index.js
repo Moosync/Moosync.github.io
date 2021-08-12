22c35f69c344570ca211daf9ccae45fec4c429c8
ovenoboyo <ovenoboyo@gmail.com>
2021-08-12 17:38:47 +0530

    use lastfm instead of youtube

---
+++ lastfmcallback/index.js
@@ -0,0 +1,3 @@
+if (window.location.pathname.includes('lastfmCallback')) {
+    open('com.moosync://lastfmCallback' + window.location.search)
+}
--- ytoauth2callback/index.js
+++
@@ -1,3 +0,0 @@
-if (window.location.pathname.includes('ytoauth2callback')) {
-    open('moosync://ytoauth2callback' + window.location.search)
-}
