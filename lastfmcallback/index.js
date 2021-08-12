7d36ff90498539e434a97e3568c4a94c5447ddc3
ovenoboyo <ovenoboyo@gmail.com>
2021-08-12 17:41:40 +0530

    test

--- lastfmcallback/index.js
+++ lastfmcallback/index.js
@@ -1,3 +1,7 @@
 if (window.location.pathname.includes('lastfmCallback')) {
     open('com.moosync://lastfmCallback' + window.location.search)
 }
+
+function test () {
+    open('com.moosync://lastfmCallback' + window.location.search);
+}
