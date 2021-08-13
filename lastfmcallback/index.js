0980a6225f2dc9a54774079caa55a08a8085ad09
ovenoboyo <ovenoboyo@gmail.com>
2021-08-14 02:01:30 +0530

    Remove unnecessary directory

--- lastfmcallback/index.js
+++
@@ -1,7 +0,0 @@
-if (window.location.pathname.includes('lastfmCallback')) {
-    open('com.moosync://lastfmCallback' + window.location.search)
-}
-
-function test () {
-    open('com.moosync://lastfmCallback' + window.location.search);
-}
