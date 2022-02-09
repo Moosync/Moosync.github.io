3b7fd6ac8ff8d38815f8b5e65f15f6998e39acb6
Sahil Gupte <sahilsachingupte@gmail.com>
2022-02-09 14:41:24 +0530

    Launch moosync protocol on predefined paths
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+import './script.js'
+import { getProviderFromURL, getProviderRedirectURL, getQueryParams } from './locationUtils.js'
+
+const providerMatch = getProviderFromURL()
+if (providerMatch && providerMatch.length > 0) {
+  const redirectPath = getProviderRedirectURL(providerMatch[0])
+
+  window.location.href = 'moosync://' + redirectPath + getQueryParams()
+}
+
